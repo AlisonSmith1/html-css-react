f708a015c783033308806ddeff2ef1e1cdcb57e8
Lana <kainingyang22@gmail.com>
2025-07-09 12:51:19 +0800

    合併前後端檔案

---
+++ interview/server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes").auth;
+const commodityRoute = require("./routes").commodity;
+const passport = require("passport");
+require("./config/passport")(passport);
+const cors = require("cors");
+const path = require("path");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("連接到MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/user", authRoute);
+app.use(
+  "/api/commodity",
+  passport.authenticate("jwt", { session: false }),
+  commodityRoute
+);
+
+//port 3000是react預設port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`後端伺服器正
