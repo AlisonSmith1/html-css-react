6ef9dc1c011cb54a16e02889ad55fe15988871a3
Lana <kainingyang22@gmail.com>
2025-07-09 20:45:52 +0800

    再一次build

--- backend/server/index.js
+++ backend/server/index.js
@@ -34,8 +34,8 @@ app.use(
 // 放在所有 API 路由之後
 app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../frontend/build/index.html"));
+app.get("/app", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 
 // 正確載入
